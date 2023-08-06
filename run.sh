a4f163d23094dcde74dc84e230c235c3fda4dbc4
naufaladrna08 <naufaladrn90@gmail.com>
2023-08-06 20:09:06 +0700

    remove make since we use cmake to build

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 set -e
 
-make all
-
 XEPHYR=$(whereis -b Xephyr | cut -f2 -d' ')
+
 xinit ./xinitrc -- \
-    "$XEPHYR" \
-        :100 \
-        -ac \
-        -screen 800x600 \
-        -host-cursor
+  "$XEPHYR" \
+  :100 \
+  -ac \
+  -screen 800x600 \
+  -host-cursor
